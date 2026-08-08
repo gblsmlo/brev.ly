f7def29b8e8426c80187025189380643c95612c7
Gabriel Melo <gmelo@bondingai.io>
2026-08-07 21:02:19 -0300

    chore(server): simplify health handler parameter

--- server/src/http/handlers/health-handler.ts
+++ server/src/http/handlers/health-handler.ts
@@ -3,7 +3,7 @@ import type { RouteHandler } from 'fastify'
 import { type GetHealthUseCase, getHealth } from '../../use-cases/get-health'
 
 export function makeHealthHandler(getHealthUseCase: GetHealthUseCase): RouteHandler {
-  return async (_request, reply) => reply.status(200).send(getHealthUseCase())
+  return async (_, reply) => reply.status(200).send(getHealthUseCase())
 }
 
 export const healthHandler = makeHealthHandler(getHealth)
